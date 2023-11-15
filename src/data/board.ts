aac6f76732df1a972f15b9912ea998ba21842014
AlexBodnarC <137523728+AlexBodnarC@users.noreply.github.com>
2023-11-15 12:37:37 +0200

    Add styles logic to actions

--- src/data/board.ts
+++ src/data/board.ts
@@ -4,8 +4,9 @@ function createData(
     link: string,
     source: string,
     action: string,
+    className: string,
 )  {
-    return { id, date_removed, link, source, action };
+    return { id, date_removed, link, source, action, className };
 }
   
 export const table_rows = [
@@ -14,69 +15,79 @@ export const table_rows = [
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Google',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Twitter',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Twitter',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148125, 
         '23 Sep 2023',
         'https://google.com',
         'Twitter',
-        'Delisted'
+        'Delisted',
+        'delisted'
     ),
     createData(
         148225, 
         '22 Sep 2023',
         'https://twitter.com',
         'Twitter',
-        'Removed'
+        'Removed',
+        'removed'
     ),
 ];
