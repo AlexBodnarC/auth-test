fb2d5597861c6b1b9ef3c16b3a52b5bb34e83640
AlexBodnarC <137523728+AlexBodnarC@users.noreply.github.com>
2023-11-15 12:39:27 +0200

    Update stepper view

--- src/components/auth/stepper/Stepper.tsx
+++ src/components/auth/stepper/Stepper.tsx
@@ -5,6 +5,8 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Typography from '@mui/material/Typography';
 import { StepContext } from '../../../contexts/step.context';
+import DoneIcon from '@mui/icons-material/Done';
+import { StepIconProps } from '@mui/material/StepIcon';
 
 const steps = [
   {
@@ -22,12 +24,21 @@ const steps = [
 export default function VerticalLinearStepper() {
   const {activeStep } = React.useContext(StepContext)
 
+  const LabelIcon = (props: StepIconProps) => {
+
+    if(props.active){
+      return <span className='stepLabel-root__active'>{props.icon}</span>
+    }
+
+    return <span className='stepLabel-root'>{props.icon}</span>;
+  };
+
   return (
     <Box sx={{ maxWidth: 400 }}>
       <Stepper activeStep={activeStep} orientation="vertical">
         {steps.map((step) => (
           <Step key={step.id}>
-            <StepLabel sx={{color: 'white'}}>
+            <StepLabel sx={{color: 'white'}} StepIconComponent={LabelIcon}>
               <Typography sx={{fontSize: '20px', color: 'white', fontWeight: '500'}}>{step.label}</Typography>
               <Typography sx={{fontSize: '12px', color: '#80b6f5'}}>{step.description}</Typography>
             </StepLabel>
